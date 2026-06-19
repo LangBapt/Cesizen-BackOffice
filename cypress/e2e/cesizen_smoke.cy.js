4e282c70c5e8df9c11499fb13ed3c8db8312b55c
LangBapt <baptistelanglois0@gmail.com>
2026-06-19 11:12:44 +0200

    [FIX-01] Ajustement de l'interface et integration de la suite de tests Cypress

---
+++ cypress/e2e/cesizen_smoke.cy.js
@@ -0,0 +1,15 @@
+describe('CesiZEN - Vérification de la page d\'accueil', () => {
+  
+  it('Doit charger correctement le Backoffice de Staging', () => {
+    cy.visit('https://cesizen-baptiste.switzerlandnorth.cloudapp.azure.com/staging')
+
+    cy.contains(/connexion|se connecter|email/i).should('be.visible')
+  })
+
+  it('Doit afficher correctement les éléments du formulaire', () => {
+    cy.visit('https://cesizen-baptiste.switzerlandnorth.cloudapp.azure.com/staging')
+    
+    cy.get('input[type="email"]').should('be.visible')
+    cy.get('button[type="submit"]').should('be.visible')
+  })
+})
